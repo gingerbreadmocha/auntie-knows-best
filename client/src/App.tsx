b1294792136cee32ab264c4e6b75cecfe0c48799
Jenny Zhou <jenny.zhou@iheartjane.com>
2026-09-13 20:24:16 -0700

    Add sidebar for project intro

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,13 @@
 import { Chatbox } from "./components/Chatbox";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-violet-100">
-      <Chatbox />
+    <div className="flex h-screen bg-violet-100">
+      <Sidebar />
+      <main className="flex-1 flex items-center justify-center p-6">
+        <Chatbox />
+      </main>
     </div>
   );
 }
